ab61319bba8dfa51aec1987862c189a15a156cdc
Darkness3840 <khizar3840@gmail.com>
2015-02-25 12:58:16 -0600

    Update defense.java

--- src/main/java/io/github/sircat/classes/defense.java
+++ src/main/java/io/github/sircat/classes/defense.java
@@ -1,1 +1,15 @@
+package io.github.sircat.classes;
 
+import net.minecraft.server.v1_8_R1.ItemStack;
+
+import com.sucy.skill.api.classes.RPGClass;
+
+public class defense extends RPGClass
+{
+	public static String NAME = "Defender";
+	
+	public Fighter(String name, ItemStack icon, int maxLevel, String parent)
+	{
+		super(NAME, null, 30, null);
+	}
+}
