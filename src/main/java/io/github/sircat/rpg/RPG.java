a76bab4a2aedd216fde1a3d39e70160584c73fe5
SirCat <micr2@mcverse.net>
2015-02-26 19:33:21 +0100

    New onEnable and onDisable

--- src/main/java/io/github/sircat/rpg/RPG.java
+++ src/main/java/io/github/sircat/rpg/RPG.java
@@ -12,16 +12,27 @@ import com.sucy.skill.api.SkillPlugin;
 
 public class RPG extends JavaPlugin implements SkillPlugin
 {
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_BLACK = "\u001B[30m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_YELLOW = "\u001B[33m";
+	public static final String ANSI_BLUE = "\u001B[34m";
+	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static final String ANSI_CYAN = "\u001B[36m";
+	public static final String ANSI_WHITE = "\u001B[37m";
+	public static final String ANSI_BOLD = "\u001B[1m";
+	
 	@Override
     public void onEnable() 
     {
-    	getLogger().info("[MCVRPG] Plugin enabled!");
+    	getLogger().info(ANSI_BOLD + ANSI_BLUE + "[MCVRPG]" + ANSI_RESET + ANSI_GREEN + "Plugin enabled! Oh boy, oh boy, fun fun fun");
     }
  
     @Override
     public void onDisable() 
     {
-        getLogger().info("[MCVRPG] Back to serious mode");
+        getLogger().info(ANSI_BOLD + ANSI_BLUE + "[MCVRPG]" + ANSI_RESET + ANSI_RED + "Back to serious mode");
     }
 
     @Override
