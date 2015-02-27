d9828bc6bcd75134b083d6300c6d3cb7f753ff3d
Darkness3840 <khizar3840@gmail.com>
2015-02-26 18:42:44 -0600

    Update and rename Defence.java to Defense.java

--- src/main/java/io/github/sircat/classes/tier1/Defense.java
+++ src/main/java/io/github/sircat/classes/tier1/Defense.java
@@ -3,11 +3,11 @@ package io.github.sircat.classes.tier1;
 
 import com.sucy.skill.api.classes.RPGClass;
 
-public class Defence extends RPGClass
+public class Defense extends RPGClass
 {
 	public static String NAME = "Defender";
 	
-	public Defence() 
+	public Defense() 
 	{
 		super(NAME, null, 30, null);
 	}	
